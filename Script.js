748954f5cce4a2750c5c1ebff0333de940ffe701
alyssals24 <alyssalsnz@gmail.com>
2024-04-14 15:10:49 +1200

    added descriptions to the menu page

--- Script.js
+++ Script.js
@@ -2,6 +2,8 @@
 let pages = ["homeContent", "menuContent", "aboutContent"];
 let pizzaNames = ["Cheese pizza", "Hawaiian pizza", "Pepperoni pizza", "Supreme pizza"];
 let pizzaImages = ["cheese pizza.png", "hawaiian pizza.png", "pepperoni pizza.png", "supreme pizza.png"];
+let pizzaDesc = ["A simple delicious cheese pizza with tomato sauce.", "Our most popular pizza topped with pineapple and ham.", 
+				"A classic pizza topped with slices of pepperoni.", "A delicious pizza topped with pepperoni, capsicum, mushrooms and olives."]
 
 var myIndex = 0;
 carousel();
@@ -64,20 +66,24 @@ function getPizzas(section) {
 		section.removeChild(section.firstChild);
 	}
 	for (let i = 0; i < pizzaNames.length; i++) {
-		showPizza(pizzaNames[i], pizzaImages[i]);
+		showPizza(pizzaNames[i], pizzaImages[i], pizzaDesc[i]);
 	}
 }
 
-function showPizza(pizzaName, pizzaImage) {
+function showPizza(pizzaName, pizzaImage, pizzaDesc) {
 	const name = document.createElement('p');
 	const image = document.createElement('img');
+	const description = document.createElement('p');
 	const pizzaSection = document.createElement('section');
 	const section = document.getElementById('menuSection');
 	name.textContent = pizzaName;
 	image.src = pizzaImage;
 	image.setAttribute('width', '170');
 	image.setAttribute('height', '150');
+	description.textContent = pizzaDesc;
+	description.setAttribute('font-size', '96px');
 	section.appendChild(pizzaSection);
-	pizzaSection.appendChild(name);
 	pizzaSection.appendChild(image);
+	pizzaSection.appendChild(name);
+	pizzaSection.appendChild(description);
 }
