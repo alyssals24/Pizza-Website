a90ccc5128f194d6647128f0bcb63e8a4e56c97e
alyssals24 <alyssalsnz@gmail.com>
2024-04-14 15:31:30 +1200

    deleted extra files and added comments

--- Script.js
+++ Script.js
@@ -5,6 +5,7 @@ let pizzaImages = ["cheese pizza.png", "hawaiian pizza.png", "pepperoni pizza.pn
 let pizzaDesc = ["A simple delicious cheese pizza with tomato sauce.", "Our most popular pizza topped with pineapple and ham.", 
 				"A classic pizza topped with slices of pepperoni.", "A delicious pizza topped with pepperoni, capsicum, mushrooms and olives."]
 
+// Rotates the pizza images
 var myIndex = 0;
 carousel();
 
@@ -17,15 +18,15 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) { myIndex = 1 }
     x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 3500);
+    setTimeout(carousel, 3500); // timer on ~3.5 seconds
 }
 
+// Changes the current page to the home page
 function changeToHome() {
-	//document.getElementById("sectionHeader").innerHTML = "Papa's Pizzeria";
 	let currentPage = pages.indexOf("homeContent");
 	let element = document.getElementById("homeContent");
-	element.removeAttribute("hidden", "hidden");
-	for (let i = 0; i < pages.length; i++) {
+	element.removeAttribute("hidden", "hidden"); // removes the hidden attribute from home content so that its visible
+	for (let i = 0; i < pages.length; i++) { // runs through pages list and hides everything that is not on the home page content
 		if (i != currentPage) {
 			element = document.getElementById(pages[i]);
 			element.setAttribute("hidden", "hidden");
@@ -33,8 +34,8 @@ function changeToHome() {
 	}
 }
 
+// Changes the current page to the menu page
 function changeToMenu() {
-	//document.getElementById("sectionHeader").innerHTML = "Menu";
 	let currentPage = pages.indexOf("menuContent");
 	let element = document.getElementById("menuContent");
 	const section = document.getElementById('menuSection');
@@ -45,11 +46,11 @@ function changeToMenu() {
 			element.setAttribute("hidden", "hidden");
 		}
 	}
-	getPizzas(section);
+	getPizzas(section); // gets pizza content for menu page to display
 }
 
+// Changes the current page to the about page
 function changeToAbout() {
-	//document.getElementById("sectionHeader").innerHTML = "About Us";
 	let currentPage = pages.indexOf("aboutContent");
 	let element = document.getElementById("aboutContent");
 	element.removeAttribute("hidden", "hidden");
@@ -61,15 +62,17 @@ function changeToAbout() {
 	}
 }
 
+// Gets the pizza names, descriptions and images to display on menu page
 function getPizzas(section) {
-	while (section.firstChild) {
+	while (section.firstChild) { // removes all pizza content if the menu page has been opened before. prevents duplicates
 		section.removeChild(section.firstChild);
 	}
 	for (let i = 0; i < pizzaNames.length; i++) {
-		showPizza(pizzaNames[i], pizzaImages[i], pizzaDesc[i]);
+		showPizza(pizzaNames[i], pizzaImages[i], pizzaDesc[i]); // sets up the pizza content
 	}
 }
 
+// Uses the given variables to create a section about a pizza and displays it on the menu page
 function showPizza(pizzaName, pizzaImage, pizzaDesc) {
 	const name = document.createElement('p');
 	const image = document.createElement('img');
@@ -81,8 +84,7 @@ function showPizza(pizzaName, pizzaImage, pizzaDesc) {
 	image.setAttribute('width', '170');
 	image.setAttribute('height', '150');
 	description.textContent = pizzaDesc;
-	description.setAttribute('font-size', '96px');
-	section.appendChild(pizzaSection);
+	section.appendChild(pizzaSection); // appends pizza content to the main menu page body
 	pizzaSection.appendChild(image);
 	pizzaSection.appendChild(name);
 	pizzaSection.appendChild(description);
